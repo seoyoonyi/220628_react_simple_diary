f671e4e8fb233572800266dd13cd373e8429ccfb
seoyoonyi <yiseoyoon@gmail.com>
2022-06-29 15:52:32 +0900

    220629 study retry

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import DiaryEditor from './DiaryEditor';
+import DiaryList from './DiaryList';
 import './App.css';
+
+const dummyList = [
+  {
+    id: 1,
+    author: '이정환',
+    content: '하이 1',
+    emotion: 1,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    author: '이정환2',
+    content: '하이 2',
+    emotion: 2,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 3,
+    author: '이정환3',
+    content: '하이 3',
+    emotion: 3,
+    created_date: new Date().getTime(),
+  },
+];
 
 function App() {
   return (
     <div className="App">
       <DiaryEditor />
+      <DiaryList diaryList={dummyList} />
     </div>
   );
 }
